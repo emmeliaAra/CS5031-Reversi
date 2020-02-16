4744a3447b9b88a87e007c0add3056a952e35ce4
emmeliaAra <emmelia.ara42@gmail.com>
2020-02-16 17:15:22 +0000

    Added Test to test that the WHITE player cannot start the game

--- src/test/java/stacs/arcade/reversi/ReversiModelAdditionalTests.java
+++ src/test/java/stacs/arcade/reversi/ReversiModelAdditionalTests.java
@@ -20,6 +20,7 @@ public class ReversiModelAdditionalTests {
 
     @Test
     public void initialMovesWillNotCapture() throws IllegalMoveException {
+
         this.model.makeMove(BLACK,4,3);
         assertEquals(1,this.model.getNoBlackStones());
         assertEquals(0,this.model.getNoWhiteStones());
@@ -39,5 +40,10 @@ public class ReversiModelAdditionalTests {
         assertEquals(2,this.model.getNoBlackStones());
         assertEquals(BLACK,this.model.getAt(4,3));
         assertEquals(BLACK,this.model.getAt(3,4));
+    }
+
+    @Test
+    public void mustRejectWhiteAsFirstPlayer() {
+        assertThrows(IllegalMoveException.class,() -> this.model.makeMove(WHITE,4,3));
     }
 }
